8577e1482e4d9d2b49038ed72c63a4020d5c6c2f
MikaelHuang <Mikael.wijaya.oey@gmail.com>
2025-07-03 09:00:23 +0700

    fix: remove submodule titan completely and add as regular folder

---
+++ titan/stackbit.config.ts
@@ -0,0 +1,18 @@
+﻿import { defineStackbitConfig } from "@stackbit/types";
+import GitContentSource from "@stackbit/cms-git";
+
+export default defineStackbitConfig({
+  stackbitVersion: "~0.6.0",
+  contentSources: [
+    new GitContentSource({
+      rootPath: __dirname,
+      contentDirs: ["content"]
+    })
+  ],
+  modelExtensions: [
+    { name: "Page", type: "page", urlPath: "/{slug}" },
+    { name: "Post", type: "page", urlPath: "/blog/{slug}" }
+    // Tambahkan model lain jika perlu, misal:
+    // { name: "Team", type: "page", urlPath: "/team/{slug}" }
+  ]
+});
